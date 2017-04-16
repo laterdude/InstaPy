0bd2ffd2a5383746034ad9b43cd1607d73435611
laterdude <rtippetts@blm.gov>
2017-04-15 23:38:22 -0600

    Update quickstart.py

--- quickstart.py
+++ quickstart.py
@@ -20,26 +20,26 @@
 #   .end()
 
 
-"""Example Case of the Script"""
+#"""Example Case of the Script"""
 from instapy import InstaPy
 
 #if you don't provide arguments, the script will look for INSTA_USER and INSTA_PW in the environment
 session = InstaPy()
 
-"""Logging in"""
+#"""Logging in"""
 #logs you in with the specified username and password
 session.login()
 
-"""Comment util"""
+#"""Comment util"""
 #default enabled=False, ~ every 4th image will be commented on
 session.set_do_comment(enabled=True, percentage=25)
 session.set_comments(['Awesome', 'Really Cool', 'I like your stuff'])
 
-"""Follow util"""
+#"""Follow util"""
 #default enabled=False, follows ~ every 10th user from the images
 session.set_do_follow(enabled=True, percentage=10)
 
-"""Image Check with Image tagging api"""
+#"""Image Check with Image tagging api"""
 #default enabled=False , enables the checking with the clarifai api (image tagging)
 #if secret and proj_id are not set, it will get the environment Variables
 # 'Clarifai_SECRET' and 'CLARIFAI_ID'
@@ -60,7 +60,7 @@ session.clarifai_check_img_for(['nsfw'], comment=False) # !if no tags are set, u
 # session.clarifai_check_img_for(['food', 'lunch'], comment=True, comments=['Tasty!', 'Yum!'])
 session.clarifai_check_img_for(['dog', 'cat', 'cute', 'horse'], comment=True, comments=['Sweet!', 'So great!'])
 
-"""Like util"""
+#"""Like util"""
 #searches the description for the given words and won't
 # like the image if one of the words are in there
 # session.set_dont_like(['food', 'eat', 'meal'])
@@ -68,11 +68,11 @@ session.clarifai_check_img_for(['dog', 'cat', 'cute', 'horse'], comment=True, co
 # one of the given words
 # session.set_ignore_if_contains(['glutenfree', 'french', 'tasty'])
 
-"""Unfollow util"""
+#"""Unfollow util"""
 #will prevent commenting and unfollowing your good friends
 # session.set_dont_include(['friend1', 'friend2', 'friend3'])
 
-"""Different tasks"""
+#"""Different tasks"""
 # you can put in as much tags as you want, likes 100 of each tag
 session.like_by_tags(['#test'], amount=100)
 
@@ -82,6 +82,6 @@ session.like_from_image(url='www.instagram.com/image', amount=100)
 session.unfollow_users(amount=10) #unfollows 10 of the accounts your following -> instagram will only unfollow 10 before you'll be 'blocked
 #  for 10 minutes' (if you enter a higher number than 10 it will unfollow 10, then wait 10 minutes and will continue then)
 
-"""Ending the script"""
+#"""Ending the script"""
 #clears all the cookies, deleting you password and all information from this session
 session.end()
